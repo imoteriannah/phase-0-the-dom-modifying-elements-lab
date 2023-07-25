4487787176d648d31d677912101b4087111e6a5d
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-07-25 12:31:23 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector("main#main").remove();
+
+const h1 = document.createElement("h1");
+h1.setAttribute("id", "victory");
+h1.innerHTML = "YOUR-NAME is the champion";
+document.querySelector("body").appendChild(h1);
+
+const newHeader = document.querySelector("h1#victory");
